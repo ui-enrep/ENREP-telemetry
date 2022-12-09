85adb61217e0c452120a0e9a207260220f82d635
Kaitlyn Strickfaden <42983916+kaitlynstrickfaden@users.noreply.github.com>
2022-12-09 11:55:48 -0800

    Add deploy.R
    Add script to deploy local version of app to Shiny. Just need to find the secret on shinyapps.io (top right under "Tokens") and change the app directory to the local directory to the app.

---
+++ deploy.R
@@ -0,0 +1,14 @@
+
+
+rsconnect::setAccountInfo(name = 'enrep', 
+                          token = '04AF4ADD8F2B1F14CA7CBAB90647E725', 
+                          secret = '<SECRET>')
+
+rsconnect::deployApp(
+  appDir = "C://Users/stri7929/Documents/GitHub/GOES_Data_Viewer_Shiny_App-main",
+  appFiles = NULL #, you can specify which files to deploy, 
+  #or keep this NULL to deploy everything
+  ,
+  account = "enrep",
+  appName = "GOES_Data_Viewer_Shiny_App",
+  appTitle = "ENREP GOES Data Viewer")
