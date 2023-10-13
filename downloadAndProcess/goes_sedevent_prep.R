9b66f070ef5f24b24f19bf6e1039bb07e0eb3649
ihellman <ian.hellman@gmail.com>
2023-10-12 19:41:48 -0700

    changed nesid key to include basinpair

--- downloadAndProcess/goes_sedevent_prep.R
+++ downloadAndProcess/goes_sedevent_prep.R
@@ -12,19 +12,19 @@ library(lubridate)
 sedDataRaw <- system("/home/ihellman/enrep/LrgsClient/bin/getDcpMessages -h lrgseddn1.cr.usgs.gov -u enrep1 -f /home/ihellman/enrep/LrgsClient/MessageBrowser_SedEvent.sc -P yrqy-XWZB-96",
                      intern = TRUE)
 
-
-# a "key" to tie the NESID value to our naming convention
-sedSiteKey <- c(
-  ".*EE300578.*" = "SS" ,
-  ".*EE300BAA.*" = "SN" ,
-  ".*EE30160E.*" = "BN" ,
-  ".*EE3018DC.*" = "BS" ,
-  ".*EE302394.*" = "TE" ,
-  ".*EE302D46.*" = "TW" ,
-  ".*EE3030E2.*" = "FW" ,
-  ".*EE303E30.*" = "FE" ,
-  ".*EE304672.*" = "CW" ,
-  ".*EE3048A0.*" = "CE"
+# Key to relate nesid to ENREP naming conventions
+sedSiteKey <- tribble(
+  ~nesid,  ~stationID, ~basinPair, 
+  "EE300578", "SS" , "Springdale" , 
+  "EE300BAA", "SN" , "Springdale" , 
+  "EE30160E", "BN" , "Blue Grouse" ,
+  "EE3018DC", "BS" , "Blue Grouse" ,
+  "EE302394", "TE" , "Tripps Knob" ,
+  "EE302D46", "TW" , "Tripps Knob" ,
+  "EE3030E2", "FW" , "Fish Creek" ,
+  "EE303E30", "FE" , "Fish Creek" ,
+  "EE304672", "CW" , "Coxit" ,
+  "EE3048A0", "CE" , "Coxit"
 )
 
 # Convert single sedDataRaw string to slightly cleaner vector of strings 
@@ -36,10 +36,8 @@ sedDataCleanVector <- sedDataRaw %>%
   str_remove('"|/') %>%
   stringi::stri_remove_empty()
 
-
-
-# Mega cleaning to final table. Arguably too much in one run.  
-sedDataClean <- tibble(raw = sedDataCleanVector) %>%
+# Main cleaning
+sedDataClean  <- tibble(raw = sedDataCleanVector) %>%
   separate(raw, into = c("id_and_date", "data"), sep = " 0 ") %>%
   filter(data != "0") %>%
   separate(id_and_date, into = c("nesid", "yy", "day", NA), sep = c(8,10,13,19), remove = TRUE) %>%
@@ -49,9 +47,12 @@ sedDataClean <- tibble(raw = sedDataCleanVector) %>%
   # -1 from date because R treats origin as Julian day 0 
   # while sed station treats it as Julian day 1
   mutate(date = as_date(as.numeric(day) - 1, origin = ymd(paste0(yy, "-01-01", sep = "")))) %>%
-  unite("datetimeUTC", c("date", "datetimeUTC"), remove = TRUE, sep = " " ) %>%
-  mutate(stationID = str_replace_all(string = nesid, pattern = sedSiteKey)) %>%
-  select(stationID, datetimeUTC, voltage_V, h2Temp_C, stage_ft, LSU) %>%
+  unite("datetimeUTC", c("date", "datetimeUTC"), remove = TRUE, sep = " " )
+
+# Add field for basinPair and stationID and some more cleaning.  
+sedDataClean <- sedDataClean %>%
+  left_join(sedSiteKey, by = "nesid") %>%
+  select(stationID, datetimeUTC, voltage_V, h2Temp_C, stage_ft, LSU, basinPair) %>%
   mutate(voltage_V = as.numeric(voltage_V),
          h2Temp_C = as.numeric(h2Temp_C),
          stage_ft = as.numeric(stage_ft),
